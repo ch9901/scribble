ef00a189b07c43b337ab2c886cb0269c92f7e3fb
ch9901 <jcy990113@gmail.com>
2025-07-18 17:33:33 +0900

    feat: section-2진행 중

--- js/script.js
+++ js/script.js
@@ -2,8 +2,18 @@ document.addEventListener("DOMContentLoaded", () => {
   new fullpage("#fullpage", {
     licenseKey: "gplv3-license", // 무료 라이선스 키
     autoScrolling: true,
-    navigation: true,
+    // navigation: true,
     anchors: ["section1", "section2", "section3"],
     scrollHorizontally: true,
+    onLeave: function (origin, destination, direction) {
+      const header = document.querySelector("header");
+
+      if (destination.index === 1) {
+        // 두 번째 섹션에 진입하면
+        header.classList.add("section2");
+      } else {
+        header.classList.remove("dark");
+      }
+    },
   });
 });
